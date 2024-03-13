78e98bc789dbd74c3062c9797dcf0f740fe5eb66
mombe <h220462g@hit.ac.zw>
2024-03-13 22:27:49 +0200

    implementing security

---
+++ Lecturer-s-Companion/src/main/java/com/Hit200/Lecturer/s/Companion/Models/user.java
@@ -0,0 +1,16 @@
+package com.Hit200.Lecturer.s.Companion.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class user {
+
+
+
+}
