c2ecb7b3e1eaa03d0ac931bc492d879f0323d9e4
mombe <h220462g@hit.ac.zw>
2024-03-13 22:29:50 +0200

    user model implemented

--- Lecturer-s-Companion/src/main/java/com/Hit200/Lecturer/s/Companion/Models/user.java
+++ Lecturer-s-Companion/src/main/java/com/Hit200/Lecturer/s/Companion/Models/user.java
@@ -1,16 +1,62 @@
 package com.Hit200.Lecturer.s.Companion.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Setter;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
 
 @Setter
 @Getter
 @Entity
 @Table(name = "users")
-public class user {
+public class user implements UserDetails {
 
 
+    @Getter
+    @Id
+    private String username;
+    @Getter
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String emailAddress;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Return the user's authorities (roles)
+        // Example: return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+        return null;
+    }
+
+
+    public boolean isAccountNonExpired() {
+        return true; // Modify as needed
+    }
+
+
+    public boolean isAccountNonLocked() {
+        return true; // Modify as needed
+    }
+
+
+    public boolean isCredentialsNonExpired() {
+        return true; // Modify as needed
+    }
+
+
+    public boolean isEnabled() {
+        return true; // Modify as needed
+    }
+
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 
 }
